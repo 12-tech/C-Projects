30b62472aef67037f98a74b0a3977dde7fac0f28
Ajoke Sulaimon <sulaimonajoke@yahoo.com>
2020-10-25 16:51:42 +0100

    my first C#

---
+++ learning_with_Frank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace learning_with_Frank
+{
+    class Program
+    {
+        static string name = "Ajoke";
+        static string first = "Hello World, \n \n I am learning with ";
+        static string last = " and it's quite interesting!";
+        static int a = 520;
+        static int b = 310;
+        static int result = a + b;
+
+        static string mid = $" we did addition of {a} + {b}  = {result} ";
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(first + name + last + mid);
+           
+        }
+    }
+}
